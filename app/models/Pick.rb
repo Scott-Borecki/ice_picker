76866b9607ea432295bc12107c7953f2515b2a19
Scott Borecki <scottborecki@gmail.com>
2021-07-24 16:47:14 -0600

    Add .random

--- app/models/Pick.rb
+++ app/models/Pick.rb
@@ -1,3 +1,7 @@
 class Pick < ApplicationRecord
   validates :who_goes_first, presence: true
+
+  def self.random
+    all.sample.who_goes_first
+  end
 end
--- spec/models/pick_spec.rb
+++ spec/models/pick_spec.rb
@@ -4,4 +4,25 @@ RSpec.describe Pick do
   describe 'validations' do
     it { should validate_presence_of(:who_goes_first) }
   end
+
+  describe 'class methods' do
+    describe '.random' do
+      it 'returns a random pick' do
+        who_goes_first_collection = [
+          'Birthday: Day of month (earliest to latest)',
+          'Birthday: Day of month (latest to earliest)',
+          'First Name: Last letter (from a to z)',
+          'First Name: Last letter (from z to a)'
+        ]
+
+        who_goes_first_collection.each_with_index do |who_goes_first|
+          Pick.create(who_goes_first: who_goes_first)
+        end
+
+        actual = Pick.random
+
+        expect(who_goes_first_collection).to include(actual)
+      end
+    end
+  end
 end
